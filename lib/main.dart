fdea0b3065530177c98e8b50ec20da28368ff8d6
dimonttt <olga.dimont@yandex.ru>
2022-02-04 17:36:50 +0300

    second comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'fooderlich_theme.dart';
+import 'home.dart';
 
 
 void main() {
@@ -15,29 +16,11 @@ class Fooderlich extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
 
-    // TODO: Apply Home widget
-    // 3
     return MaterialApp(
       theme: theme,
-
       title: 'Fooderlich',
-      // 4
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-
-        appBar: AppBar(title: const Text('Fooderlich')),
-        body: Center(
-          child: Text('Let\'s get cooking 👩‍🍳',
-              style: theme.textTheme.headline1),
-        ),
-
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
-      ),
+      home: const Home(),
     );
+
   }
 }
